66e1b2dc077f00bb6b8b01cac31a22ea597941a0
Nada Rahmawati <haekalboedoet@gmail.com>
2022-12-11 01:20:25 +0700

    setting_edit_profile_form

--- application/views/admin/setting_upload_avatar.php
+++ application/views/admin/setting_upload_avatar.php
@@ -11,41 +11,43 @@
 
 		<div id="layoutSidenav_content">
 			<div class="container-fluid px-4">
-                <h1 class="mt-4">Upload Avatar</h1>
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Upload Avatar</li>
-                    </ol>
+				<h1 class="mt-4">Upload Avatar</h1>
+				<ol class="breadcrumb mb-4">
+					<li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+					<li class="breadcrumb-item active">Upload Avatar</li>
+				</ol>
 
-			
-					<div class="row">
-                          <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                    Upload Avatar
-                                    </div>
-                                    <div class="card-body">
-									<form action="" method="POST" enctype="multipart/form-data">
-										<div>
-											<label for="avatar">Pilih Gambar Avatar</label>
-											<input class="form-control" type="file" name="avatar" id="avatar" accept="image/png, image/jpeg, image/jpg, image/gif">
-										</div>
 
-										<?php if (isset($error)) : ?>
-											<div class="invalid-feedback"><?= $error ?></div>
-										<?php endif; ?>
-										<br>
-										<div>
-											<button type="submit" name="save" class="btn btn-dark">Upload</button>
-										</div>
-									</form>
-                                  </div>
-                                </div>
-                            </div>
-                            <div class="col-xl-6">
-                                <h4>Upload Avatar</h4><p class="lead">Upload Your Avatar.</p>
-                            </div>
-                        </div>
+				<div class="row">
+					<div class="col-xl-6">
+						<div class="card mb-4">
+							<div class="card-header">
+								Upload Avatar
+							</div>
+							<div class="card-body">
+								<form action="" method="POST" enctype="multipart/form-data">
+									<div>
+										<label for="avatar">Pilih Gambar Avatar</label>
+										<input class="form-control" type="file" name="avatar" id="avatar"
+											accept="image/png, image/jpeg, image/jpg, image/gif">
+									</div>
+
+									<?php if (isset($error)) : ?>
+									<div class="invalid-feedback"><?= $error ?></div>
+									<?php endif; ?>
+									<br>
+									<div>
+										<button type="submit" name="save" class="btn btn-dark">Upload</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<h4>Upload Avatar</h4>
+						<p class="lead">Upload Your Avatar.</p>
+					</div>
+				</div>
 			</div>
 			<?php $this->load->view('admin/_partials/footer.php') ?>
 		</div>
